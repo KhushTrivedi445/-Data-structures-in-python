43f6444c437a1e6764bcc4e835d2d7847fcfc66d
Khush Trivedi <168230450+KhushTrivedi445@users.noreply.github.com>
2024-07-23 23:42:05 +0530

    Add files via upload

---
+++ stack implementation using oops.py
@@ -0,0 +1,34 @@
+#Implementation of stack using oops(class)
+
+
+class stack:
+    def __init__(self):
+        self.stack=[]
+        self.top=-1
+    def push(self,data):
+        self.top+=1
+        self.stack.append(data) 
+    def pop(self):
+        self.top-=1
+        self.stack.pop()
+        return stack
+    def peek(self):
+         print(self.stack[self.top])
+    def display(self):
+        for i in range(self.top,-1,-1):
+            print(self.stack[i])
+            print("|")
+            
+s=stack()
+
+s.push(0)
+s.push(1)
+s.push(2)
+s.push(3)
+
+#s.pop()
+
+s.peek()
+
+s.display()
+        
