1272b2e5b19a37e32eea03af7919955e7f260f2c
Khush Trivedi <168230450+KhushTrivedi445@users.noreply.github.com>
2024-07-26 00:33:13 +0530

    Add files via upload

---
+++ length of a string using class and object.py
@@ -0,0 +1,15 @@
+class stringlength:
+    def __init__(self):
+        self.str=input("Enter string")
+    def countlength(self):
+        print("The length of ",self.str,"is", len(self.str))
+        
+s1=stringlength()
+s2=stringlength()
+s3=stringlength()
+s4=stringlength()
+
+s1.countlength()
+s2.countlength()
+s3.countlength()
+s4.countlength()
